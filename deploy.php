9f33e48d48171e66b5964091600a04d5a4facb17
funmap-sync <funmap-sync@localhost>
2025-10-19 23:51:43 +1100

    Auto-sync_2025-10-19_23-51-43

--- deploy.php
+++ deploy.php
@@ -7,15 +7,20 @@ function logline($msg) {
 
 logline("Webhook triggered.");
 
+// Move to local repo folder
 chdir('/home/funmapco/repositories/github');
-exec('git reset --hard HEAD 2>&1', $out1, $ret1);
-logline("git reset: " . implode("\n", $out1));
 
-exec('git pull origin main 2>&1', $out2, $ret2);
-logline("git pull: " . implode("\n", $out2));
+// Pull latest from GitHub
+exec('git fetch origin main 2>&1', $out1, $ret1);
+logline("git fetch: " . implode("\n", $out1));
 
+exec('git reset --hard origin/main 2>&1', $out2, $ret2);
+logline("git reset: " . implode("\n", $out2));
+
+// Run sync to update public_html
 exec('/home/funmapco/auto-publish.sh 2>&1', $out3, $ret3);
 logline("auto-publish: " . implode("\n", $out3));
 
 logline("Deployment finished.\n");
 http_response_code(200);
+?>
