f4cdb680b19ef0773cdb5a4ed9aeb727ffa8b767
Zxen <zxen@hotmail.com>
2025-11-07 12:13:59 +1100

    Add new "Forms 2" tab and associated content to index.html

---
+++ formbuilder.js
@@ -0,0 +1,12 @@
+// Formbuilder.js - Extracted Form Builder functionality
+// This file will contain all form builder related code
+
+(function() {
+  'use strict';
+
+  // Formbuilder code will be extracted here in stages
+
+  console.log('Formbuilder.js loaded');
+
+})();
+
