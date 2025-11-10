208e7543d8a6191708331960bdb2082fa523e023
Zxen <zxen@hotmail.com>
2025-11-10 17:32:28 +1100

    hblklhlh

--- index.js
+++ index.js
@@ -15321,8 +15321,12 @@ function makePosts(){
             <div class="post-venue-selection-container"></div>
             <div class="post-session-selection-container"></div>
             <div class="location-section">
-              <div id="venue-${p.id}" class="venue-dropdown options-dropdown"><button class="venue-btn" aria-haspopup="true" aria-expanded="false"><span class="venue-name">${loc0.venue||''}</span><span class="address_line">${loc0.address||''}</span>${locationList.length>1?'<span class="results-arrow" aria-hidden="true"></span>':''}</button><div class="venue-menu post-venue-menu" hidden><div class="map-container"><div id="map-${p.id}" class="post-map"></div></div><div class="venue-options">${locationList.map((loc,i)=>`<button data-index="${i}"><span class="venue-name">${loc.venue}</span><span class="address_line">${loc.address}</span></button>`).join('')}</div></div></div>
-              <div id="sess-${p.id}" class="session-dropdown options-dropdown"><button class="sess-btn" aria-haspopup="true" aria-expanded="false">Select Session</button><div class="session-menu options-menu" hidden><div class="calendar-container"><div class="calendar-scroll"><div id="cal-${p.id}" class="post-calendar"></div></div></div><div class="session-options"></div></div></div>
+              <div class="post-preview-container">
+                <div id="venue-${p.id}" class="venue-preview"><div id="map-preview-${p.id}" class="map-preview"></div></div>
+                <div id="sess-${p.id}" class="session-preview"><div id="cal-preview-${p.id}" class="calendar-preview"></div></div>
+              </div>
+              <div class="venue-menu post-venue-menu" data-post-id="${p.id}" hidden><div class="map-container"><div id="map-${p.id}" class="post-map"></div></div><div class="venue-options">${locationList.map((loc,i)=>`<button data-index="${i}"><span class="venue-name">${loc.venue}</span><span class="address_line">${loc.address}</span></button>`).join('')}</div></div>
+              <div class="session-menu options-menu" data-post-id="${p.id}" hidden><div class="calendar-container"><div class="calendar-scroll"><div id="cal-${p.id}" class="post-calendar"></div></div></div><div class="session-options"></div></div>
             </div>
             <div class="post-details-info-container">
               <div id="venue-info-${p.id}" class="venue-info"></div>
