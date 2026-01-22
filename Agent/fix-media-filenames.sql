80857172d69fab3478e152c9f49c3b3599786404
Zxen <zxen@hotmail.com>
2026-01-23 05:05:19 +1100

    >

---
+++ Agent/fix-media-filenames.sql
@@ -0,0 +1,18 @@
+-- Fix post_media file_name to match actual Bunny filenames
+-- The app builds URLs from file_name, so they MUST match
+
+UPDATE `post_media` SET `file_name` = '00000016-Sydney_Australia._(21339175489).jpg' WHERE `id` = 41;
+UPDATE `post_media` SET `file_name` = '00000016-Interior_of_Sydney_Opera_House_Concert_Hall_during_performance.jpg' WHERE `id` = 42;
+UPDATE `post_media` SET `file_name` = '00000016-Joan_Sutherland_Theatre_Interior.JPG' WHERE `id` = 43;
+UPDATE `post_media` SET `file_name` = '00000017-Sydney_Harbour_Bridge-2022.jpg' WHERE `id` = 44;
+UPDATE `post_media` SET `file_name` = '00000017-Sydney_Harbour_Bridge_from_the_air.JPG' WHERE `id` = 45;
+UPDATE `post_media` SET `file_name` = '00000017-Sydney_Harbour_Bridge_from_Circular_Quay.jpg' WHERE `id` = 46;
+UPDATE `post_media` SET `file_name` = '00000018-Bondi_from_above.jpg' WHERE `id` = 47;
+UPDATE `post_media` SET `file_name` = '00000018-A_rip_current_pouring_over_the_people_standing_on_rock_shore_at_the_northern_end_of_Bondi_beach.jpg' WHERE `id` = 48;
+UPDATE `post_media` SET `file_name` = '00000018-Bondi_Beach,_Sydney_(15175458494).jpg' WHERE `id` = 49;
+UPDATE `post_media` SET `file_name` = '00000019-Taronga_Park_Zoo_-7Sept2008.jpg' WHERE `id` = 50;
+UPDATE `post_media` SET `file_name` = '00000019-Rusticbridge.jpg' WHERE `id` = 51;
+UPDATE `post_media` SET `file_name` = '00000019-2022-06-25_Giraffes_in_Taronga_Zoo.jpg' WHERE `id` = 52;
+UPDATE `post_media` SET `file_name` = '00000020-Gates_at_Royal_Botanic_Gardens_viewed_from_Art_Gallery_Road.jpg' WHERE `id` = 53;
+UPDATE `post_media` SET `file_name` = '00000020-After_the_fire_-_The_Garden_Palace_(8005393749).jpg' WHERE `id` = 54;
+UPDATE `post_media` SET `file_name` = '00000020-A_view_of_Royal_Botanic_Garden_taken_from_Sydney_Tower.jpeg' WHERE `id` = 55;
