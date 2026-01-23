da659d6f9c275d8ecc0ce7c0cc60122a317d40ac
Zxen <zxen@hotmail.com>
2026-01-24 01:58:19 +1100

    >

---
+++ Agent/fix-broken-20.sql
@@ -0,0 +1,103 @@
+SET NAMES utf8mb4;
+
+UPDATE post_map_cards SET description = 'The Cardboard Cathedral, formally called the Transitional Cathedral, in Christchurch, New Zealand, is the transitional pro-cathedral of the Anglican Diocese of Christchurch, replacing ChristChurch Cathedral, which was significantly damaged in the 2011 Christchurch earthquake. It is located on the site of the former St John the Baptist Church on the corner of Hereford and Madras Streets in Latimer Square, several blocks from the permanent location of ChristChurch Cathedral.
+
+The Cardboard Cathedral was designed by the Japanese architect Shigeru Ban in association with Warren and Mahoney and opened in August 2013. The church gets its nickname from the use of over 90 large cardboard tubes for its walls and roof.
+
+Photo: 準建築人手札網站 (CC BY 2.0)' WHERE post_id = 151;
+
+UPDATE post_map_cards SET description = 'The Panthéon-Benou00eet Prieur-Panthu00e9on.wav","classes":", from Ancient Greek πάνειον (pántheion) \' to all the gods\') is a monument in the 5th arrondissement of Paris, France. It stands in the Latin Quarter (Quartier latin), atop the Montagne Sainte-Geneviève, in the centre of the Place du Panthéon, which was named after it. The edifice was built between 1758 and 1790, from designs by Jacques-Germain Soufflot, at the behest of King Louis XV; the king intended it as a church dedicated to Saint Genevieve, Paris\'s patron saint, whose relics were to be housed in the church. Neither Soufflot nor Louis XV lived to see the church completed.
+
+By the time the construction was finished, the French Revolution had started; the National Constituent Assembly voted in 1791 to transform the Church of Saint Genevieve into a mausoleum for the remains of distinguished French citizens, modelled on the Pantheon in Rome which had been used in this way since the 17th century. The first panthéonisé was Honoré Gabriel Riqueti, comte de Mirabeau, although his remains were removed from the building a few years later. The Panthéon was twice restored to church usage in the course of the 19th centuryalthough Soufflot\'s remains were transferred inside it in 1829until the French Third Republic finally decreed the building\'s exclusive use as a mausoleum in 1881. The placement of Victor Hugo\'s remains in the crypt in 1885 was its first entombment in over 50 years.
+
+Photo: Moonik (CC BY-SA 3.0); Jacques-Germain Soufflot, gravure de Charpentier. (Public domain); Jacques-Germain Soufflot (Public domain)' WHERE post_id = 192;
+
+UPDATE post_map_cards SET description = 'The Stadtpark in Vienna, Austria is a large municipal park that extends from the Ringstraße in the Innere Stadt first district up to the Heumarkt (Hay Market) in the Landstraße third district. The park is divided in two sections by the Wienfluss (Vienna River), and has a total surface area of 65,000 square metres (28 acres). Scattered throughout the park are statues of famous Viennese artists, writers, and composers, including Hans Canon, Emil Jakob Schindler, Johann Strauss II, Franz Schubert, and Anton Bruckner. The opulent Kursalon building on Johannesgasse, with its broad terrace that reaches into the park, is the site of popular waltz concerts.
+
+Photo: KF at English Wikipedia (Public domain); Gryffindor (CC BY 2.5); Wiki User:唐戈 摄于维也纳市立公园 (CC BY-SA 3.0)' WHERE post_id = 299;
+
+UPDATE post_map_cards SET description = 'Cloud Gate is a public sculpture by Indian-born British artist Anish Kapoor, that is the centerpiece of Grainger Plaza at Millennium Park in the Loop community area of Chicago. Constructed between 2004 and 2006, the sculpture is nicknamed "The Bean" because of its shape, a name Kapoor later grew fond of. Made up of 168 stainless steel plates welded together, its reflective and highly polished exterior has no visible seams. It measures 33 by 66 by 42 feet (10 by 20 by 13 m), and weighs 110 short tons (100 t; 98 long tons). The sculpture and its plaza are located above Millennium Hall, between the Chase Promenade and McCormick Tribune Plaza  Ice Rink.
+
+Kapoor\'s design was inspired by liquid mercury and the sculpture\'s surface reflects and distorts the city\'s skyline and clouds moving overhead. Visitors are able to walk around and under Cloud Gate\'s 12-foot (3.7 m) high arch. On the underside is the "omphalos"-language text"ὀμφαλός \'navel\'), a concave chamber that warps and multiplies reflections. The sculpture builds upon many of Kapoor\'s artistic themes, and it is popular with tourists as a photo-taking opportunity for its unique reflective properties.
+
+Photo: А (Public domain); Vogler (CC BY-SA 4.0)' WHERE post_id = 370;
+
+UPDATE post_map_cards SET description = 'Nanzen-ji, or Zuiryusan Nanzen-ji, formerly Zenrin-ji, is a Zen Buddhist temple in Kyoto, Japan. Emperor Kameyama established it in 1291 on the site of his previous detached palace. It is also the headquarters of the Nanzen-ji branch of Rinzai Zen. The precincts of Nanzen-ji are a nationally designated Historic Site and the Hōjō gardens a Place of Scenic Beauty. The temple was destroyed in a fire in 1895 and rebuilt in 1909.
+
+Photo: Volfgang (CC BY-SA 3.0); Unknown (CC BY-SA 3.0); hiro / Ｈｉｒｏ２００６ at Japanese Wikipedia (CC BY-SA 3.0)' WHERE post_id = 434;
+
+UPDATE post_map_cards SET description = 'Wat Pho.ogg","classes":"), also spelled Wat Po, is a Buddhist temple complex in the Phra Nakhon District, Bangkok, Thailand. It is on Rattanakosin Island, directly south of the Grand Palace. Known also as the Temple of the Reclining Buddha, its official name is Wat Phra Chetuphon Wimon Mangkhalaram Rajwaramahawihan. The more commonly known name, Wat Pho, is a contraction of its older name, Wat Photaram.
+
+The temple is first on the list of six temples in Thailand classed as the highest grade of the first-class royal temples. It is associated with King Rama I who rebuilt the temple complex on an earlier temple site. It became his main temple and is where some of his ashes are enshrined. The temple was later expanded and extensively renovated by Rama III. The temple complex houses the largest collection of Buddha images in Thailand, including a 46 m long huge reclining Buddha. The temple is considered the earliest centre for public education in Thailand, and the illustrations and inscriptions placed in the temple for public instructions has been recognised by UNESCO in its Memory of the World Programme. It houses a school of Thai medicine, and is also known as the birthplace of traditional Thai massage which is still taught and practiced at the temple.
+
+Photo: Hdamm (CC BY-SA 3.0); Unknown (CC BY 4.0); กสิณธร ราชโอรส (CC BY-SA 3.0)' WHERE post_id = 476;
+
+UPDATE post_map_cards SET description = 'Wat Arun Ratchawararam Ratchawaramahawihan or Wat Arun is a Buddhist temple in the Bangkok Yai district of Bangkok, Thailand. It is situated on Thonburi on the west bank of the Chao Phraya River. The temple derives its name from the Hindu god Aruṇa, often personified as the radiations of the rising sun. Wat Arun is among the best known of Thailand\'s landmarks. Although the temple has existed since at least the 17th century, its distinctive prang (spire) was built in the early 19th century during the reigns of Rama II and Rama III.
+
+Photo: Hdamm (CC BY-SA 3.0); Jean Vollant des Verquains, architect and engineer, inspired by Vauban\'s works (Public domain); กรมประชาสัมพันธ์ สำนักนายกรัฐมนตรี (The Government Public Relations Department, Office of the Prime  (Public domain)' WHERE post_id = 477;
+
+UPDATE post_map_cards SET description = 'Namdaemun Market is a large traditional market in Seoul, South Korea. It is located next to Namdaemun, the main southern gate to the old city. The market is among the oldest extant markets in Korea, having opened during the Joseon period in 1414.
+
+The market\'s character, location, and size have all changed over time. Amidst Joseon\'s isolationism in the 16th and 17th centuries, the market was limited to mostly Korean customers and merchants. This persisted until the Empire of Japan forcefully opened Korea in the JapanKorea Treaty of 1876. Afterwards, nearly half of the merchants became either ethnic Chinese or Japanese merchants. During the 19101945 Japanese colonial period, the market survived a number of attempts by the Japanese colonial government to shut it down. The market was destroyed a number of times over time by accidental fires, including one during the 19501953 Korean War. Each time it was destroyed, it was rebuilt to roughly the same size and status it had previously operated under.
+
+Photo: 한국저작권위원회 (CC BY 4.0); 서울특별시 소방재난본부(Seoul Metropolitan Fire &amp; Disaster Headquarters) (CC BY-SA 4.0); Sgroey (CC BY-SA 4.0)' WHERE post_id = 611;
+
+UPDATE post_map_cards SET description = 'The Bolshoi Theatrer, lit. \'Grand Theater\') is a historic opera house in Moscow, Russia, originally designed by architect Joseph Bové. Before the October Revolution it was a part of the Imperial Theatres of the Russian Empire along with Maly Theatre in Moscow and a few theatres in Saint Petersburg Theatre, later Mariinsky Theatre and others).
+
+The Bolshoi Ballet and Bolshoi Opera are among the oldest and best known ballet and opera companies in the world. It is by far the world\'s biggest ballet company, with more than 200 dancers. The theatre is the parent company of The Bolshoi Ballet Academy, a leading school of ballet. It has a branch at the Bolshoi Theater School in Joinville, Brazil.
+
+Photo: DmitriyGuryanov (CC BY-SA 3.0); Арну Жан Батист(Arnout Pere, литограф)(по оригиналу Вивиена) (Public domain); Mihály Zichy (Public domain)' WHERE post_id = 647;
+
+UPDATE post_map_cards SET description = 'Shinsaibashi is a district in the Chūō-ku ward of Osaka, Japan and the city\'s main shopping area. At its center is Shinsaibashi-suji, a covered shopping street, that is north of Dōtonbori and Sōemonchō, and parallel and east of Mido-suji street. Associated with Shinsaibashi, and west of Mido-suji street, is Amerika-mura, an American-themed shopping area and center of Osaka\'s youth culture. Major stores and boutiques concentrates are found around the area. Shinsaibashi is easily accessed via the subway.
+
+Photo: Oilstreet (CC BY 2.5); Wing1990hk (CC BY 3.0); 撮影者不詳 オランダ国立ライデン大学提供 (Public domain)' WHERE post_id = 842;
+
+UPDATE post_map_cards SET description = 'Angkor National Museum is an archaeological museum in Siem Reap, Cambodia. It is dedicated to the collection, preservation and presentation of Angkorian artifacts, also to provides information and education about art and culture of Khmer civilization, with collections mainly dated from Khmer Empire\'s Angkor period circa 9th to 14th-century. Most of the artifacts are discovered in and around the Angkor archaeological sites nearby.
+
+Opened on 12 November 2007, the Angkor National Museum covers the golden era of the Khmer Empire, making use of audio-visual multimedia technology. The museum covers Khmer history, civilization, and cultural heritage in eight galleries. This museum is owned by Cambodia in ករងសមរប . It is currently displaying archaeological objects borrowed from the Cambodian National Museum in Phnom Penh. Another source of artifacts is the Conservation d\'Angkor, a storage facility of some 6,000 pieces created by the École française d\'Extrême-Orient in 1908 and currently in the hands of the Cambodian Ministry of Culture.
+
+Photo: Dltl2010 (CC BY-SA 3.0); Dltl2010 (CC BY-SA 3.0); Dltl2010 (CC BY-SA 3.0)' WHERE post_id = 1037;
+
+UPDATE post_map_cards SET description = 'Wat Phra That Doi Suthep is a Theravada Buddhist temple in Chiang Mai Province, Thailand. The temple is often referred to as "Doi Suthep" although this is actually the name of the mountain where it is located. It is a sacred site to many Thai people. The temple is 15 kilometres (9.3 mi) from the city of Chiang Mai and situated at an elevation of 1,073 meters. From the temple, impressive views of downtown Chiang Mai can be seen.
+
+Photo: JJ Harrison (https://www.jjharrison.com.au/) (CC BY-SA 3.0); ผู้สร้างสรรค์ผลงาน/ส่งข้อมูลเก็บในคลังข้อมูลเสรีวิกิมีเดียคอมมอนส์ - เทวประภาส มากคล้าย (CC BY 3.0); ErwinMeier (CC BY-SA 4.0)' WHERE post_id = 1188;
+
+UPDATE post_map_cards SET description = 'The Royal Flora Ratchaphruek was an international horticultural exposition held 1 November, 2006 to 31 January, 2007 at Mae Hia, Chiang Mai Province, Thailand that drew 3,781,624 visitors. Recognised by the Bureau International des Expositions (BIE), it was one of the grand celebrations hosted by the Royal Thai Government in honor of King Bhumibol, the world\'s longest reigning monarch.
+
+Photo: Noongsaao (CC BY-SA 3.0); Noongsaao (CC BY-SA 3.0); ผู้สร้างสรรค์ผลงาน/ส่งข้อมูลเก็บในคลังข้อมูลเสรีวิกิมีเดียคอมมอนส์ - เทวประภาส มากคล้าย (Public domain)' WHERE post_id = 1192;
+
+UPDATE post_map_cards SET description = 'Jabal Hafeet is a mountain in the region of Tawam, on the border of the United Arab Emirates and Oman. It is often considered an outlier of the Hajar Mountains in Eastern Arabia due to its proximity to the main range. To the north is the UAE city of Al Ain, in the Eastern Region of the Emirate of Abu Dhabi, and the adjacent Omani town of Al-Buraimi.
+
+The sole mountain in the Emirate of Abu Dhabi, and one of the highest mountains in the country, it has given its name to a period in UAE history, the Hafit Period of the Bronze Age (3200 to 2600 BCE), because of the discovery of a cluster of important beehive tombs at its foothills. As of 2017, the mountain is recognised as being part of a national park, and was incorporated into the Sheikh Zayed Network of Protected Areas in 2018.
+
+Photo: Nicolascornet (CC BY-SA 4.0); DEMIS Mapserver, पाटलिपुत्र (CC BY-SA 3.0); haroon_tt (CC BY-SA 3.0)' WHERE post_id = 1340;
+
+UPDATE post_map_cards SET description = 'The Center, formerly and still commonly called the CNN Center, is the former international headquarters of U.S. cable network CNN in Atlanta, Georgia, United States. The main newsrooms and studios for several of CNN\'s news channels were located in the building. The facility\'s commercial office space was occupied by various units of the former Turner Broadcasting System, now part of Warner Bros. Discovery. The Center is located in downtown Atlanta adjacent to Centennial Olympic Park.
+
+In 2020, CNN announced that it would relocate its Atlanta operations back to the Turner Broadcasting Techwood campus in Midtown Atlanta. The One CNN Center office building was acquired by CP Group in 2021. Production activities moved back in October 2023, while the last of CNN\'s domestic programs moved in February 2024.
+
+Photo: Connor.carey (CC BY-SA 4.0); þħɥʂıɕıʄʈʝɘɖı (Public domain); Katie from Memphis, TN, USA (CC BY-SA 2.0)' WHERE post_id = 1599;
+
+UPDATE post_map_cards SET description = 'Shinsaibashi is a district in the Chūō-ku ward of Osaka, Japan and the city\'s main shopping area. At its center is Shinsaibashi-suji, a covered shopping street, that is north of Dōtonbori and Sōemonchō, and parallel and east of Mido-suji street. Associated with Shinsaibashi, and west of Mido-suji street, is Amerika-mura, an American-themed shopping area and center of Osaka\'s youth culture. Major stores and boutiques concentrates are found around the area. Shinsaibashi is easily accessed via the subway.
+
+Photo: Oilstreet (CC BY 2.5); Wing1990hk (CC BY 3.0); 撮影者不詳 オランダ国立ライデン大学提供 (Public domain)' WHERE post_id = 1622;
+
+UPDATE post_map_cards SET description = 'The Nagoya Dome (ナゴヤドーム), known as Vantelin Dome Nagoya (バンテリンドーム ナゴヤ) for sponsorship reasons, is a domed baseball stadium, constructed in 1997, located in the city of Nagoya, Japan. The dome has the capacity of 36,418 for sports and 49,000 for concerts. It is an example of a geodesic dome.
+
+It has served as the home stadium for the Chunichi Dragons professional baseball team since its opening. It has also served the Orix BlueWave and Kintetsu Buffaloes, and hosted a single group stage game of the 2024 WBSC Premier12.
+
+Photo: KKPCW（Kyu3） (CC BY-SA 4.0); Adc95 (CC BY-SA 4.0)' WHERE post_id = 1637;
+
+UPDATE post_map_cards SET description = 'Nara Dreamland, or just simply Dreamland, was a theme park near Nara, Japan, heavily inspired by Disneyland in California. It opened in 1961 and was in continuous operation until its permanent closure in 2006 as a result of falling attendance due to the overwhelming popularity of newer amusement parks in Japan. The park was left abandoned until it was demolished between October 2016 and December 2017.
+
+Photo: Ivan Lucas (CC BY-SA 2.5); 国土交通省国土地理院 (Attribution)' WHERE post_id = 1655;
+
+UPDATE post_map_cards SET description = 'The Kobe Suma Sea World, formerly known as the Suma Aqualife Park(須磨海浜水族園), is a public aquarium located in Suma-ku, Kobe, Japan. Suma Aqualife Park was closed on May 31, 2023, privatized, and later reopened as Kobe Suma Sea World in June 2024 at its current location. It is a member of the Japanese Association of Zoos and Aquariums (JAZA).
+
+Photo: Haiskawa (CC BY 4.0); Mti (CC BY-SA 3.0); Jin Kemoole (CC BY 2.0)' WHERE post_id = 1660;
+
+UPDATE post_map_cards SET description = 'Yongdusan Park is a park located in Jung District, Busan, South Korea. The 120-meter-high Busan Tower is located here.
+
+The park is located on a mountain, Yongdusan. The mountain previously went by the name Songhyeonsan, and the park was briefly named Unam Park. The park occupies an area of 69,000 square meters (740,000 sq ft). It has 70 different species of trees growing in it.
+
+Photo: Mobius6 (CC BY-SA 4.0); 한국저작권위원회 (CC BY 4.0); Michiel1972 (CC BY-SA 3.0)' WHERE post_id = 1687;
