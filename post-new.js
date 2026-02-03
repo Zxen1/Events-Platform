a95289aa123c212493ed19b5bc048ece8ff87d8f
Zxen <zxen@hotmail.com>
2026-02-04 00:31:24 +1100

    >

--- post-new.js
+++ post-new.js
@@ -289,10 +289,6 @@ const PostModule = (function() {
     try { postListEl.setAttribute('tabindex', '0'); } catch (_eTab0) {}
     try { recentPanelContentEl.setAttribute('tabindex', '0'); } catch (_eTab1) {}
 
-    // Disable BottomSlack click-hold behavior inside these panels (it can block scrolling).
-    // The filter/admin/member panels are where the anti-jank spacer is actually needed.
-    try { postListEl.setAttribute('data-bottomslack', 'false'); } catch (_eSlack0) {}
-    try { recentPanelContentEl.setAttribute('data-bottomslack', 'false'); } catch (_eSlack1) {}
   }
 
   function bindAppEvents() {
