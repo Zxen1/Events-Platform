4f5c24a785c3ab1793d1ac15c76d40308717e000
Zxen <zxen@hotmail.com>
2026-02-11 00:25:13 +1100

    >

--- formbuilder.js
+++ formbuilder.js
@@ -3707,7 +3707,7 @@
         
         // Create quantity selector row
         var quantityRow = document.createElement('div');
-        quantityRow.className = 'member-postform-location-quantity-row';
+        quantityRow.className = 'member-postform-location-quantity-container';
         
         var quantityLabel = document.createElement('span');
         quantityLabel.className = 'member-postform-location-quantity-label';
