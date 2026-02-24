35673f71625fc22d2f2a3b6751f061340ea64b0b
Zxen <zxen@hotmail.com>
2026-02-24 17:37:12 +1100

    fg

--- index.php
+++ index.php
@@ -773,14 +773,6 @@ if (empty($ogTitle)) {
                   <input type="text" class="admin-settings-field-input" id="adminFolderAgeRatings" data-setting-key="folder_age_ratings" autocomplete="off" placeholder="age-ratings/" />
                 </div>
                 <div class="admin-settings-field">
-                  <label class="admin-settings-field-label" for="adminFolderLinks">Links Folder</label>
-                  <input type="text" class="admin-settings-field-input" id="adminFolderLinks" data-setting-key="folder_links" autocomplete="off" placeholder="Links/" />
-                </div>
-                <div class="admin-settings-field">
-                  <label class="admin-settings-field-label" for="adminFolderFieldsetIcons">Fieldset Icons Folder</label>
-                  <input type="text" class="admin-settings-field-input" id="adminFolderFieldsetIcons" data-setting-key="folder_fieldset_icons" autocomplete="off" placeholder="system-images/" />
-                </div>
-                <div class="admin-settings-field">
                   <label class="admin-settings-field-label" for="adminFolderAmenities">Amenities Folder</label>
                   <input type="text" class="admin-settings-field-input" id="adminFolderAmenities" data-setting-key="folder_amenities" autocomplete="off" placeholder="amenities/" />
                 </div>
@@ -793,16 +785,24 @@ if (empty($ogTitle)) {
                   <input type="text" class="admin-settings-field-input" id="adminFolderCategoryIcons" data-setting-key="folder_category_icons" autocomplete="off" placeholder="category-icons/" />
                 </div>
                 <div class="admin-settings-field">
-                  <label class="admin-settings-field-label" for="adminFolderCurrencies">Currencies Folder</label>
-                  <input type="text" class="admin-settings-field-input" id="adminFolderCurrencies" data-setting-key="folder_currencies" autocomplete="off" placeholder="currencies/" />
-                </div>
-                <div class="admin-settings-field">
                   <label class="admin-settings-field-label" for="adminFolderCountries">Countries Folder</label>
                   <input type="text" class="admin-settings-field-input" id="adminFolderCountries" data-setting-key="folder_countries" autocomplete="off" placeholder="flags/" />
                 </div>
                 <div class="admin-settings-field">
+                  <label class="admin-settings-field-label" for="adminFolderCurrencies">Currencies Folder</label>
+                  <input type="text" class="admin-settings-field-input" id="adminFolderCurrencies" data-setting-key="folder_currencies" autocomplete="off" placeholder="currencies/" />
+                </div>
+                <div class="admin-settings-field">
                   <label class="admin-settings-field-label" for="adminFolderDummyImages">Dummy Images Folder</label>
                   <input type="text" class="admin-settings-field-input" id="adminFolderDummyImages" data-setting-key="folder_dummy_images" autocomplete="off" placeholder="dummy-images/" />
+                </div>
+                <div class="admin-settings-field">
+                  <label class="admin-settings-field-label" for="adminFolderFieldsetIcons">Fieldset Icons Folder</label>
+                  <input type="text" class="admin-settings-field-input" id="adminFolderFieldsetIcons" data-setting-key="folder_fieldset_icons" autocomplete="off" placeholder="system-images/" />
+                </div>
+                <div class="admin-settings-field">
+                  <label class="admin-settings-field-label" for="adminFolderLinks">Links Folder</label>
+                  <input type="text" class="admin-settings-field-input" id="adminFolderLinks" data-setting-key="folder_links" autocomplete="off" placeholder="Links/" />
                 </div>
                 <div class="admin-settings-field">
                   <label class="admin-settings-field-label" for="adminFolderMapImages">Map Images Folder</label>
