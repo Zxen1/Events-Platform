da4a487e87af24b3eda2c91a024604dde1cabb94
Zxen <zxen@hotmail.com>
2026-04-08 01:47:38 +1000

    dyt

--- filter.js
+++ filter.js
@@ -278,8 +278,6 @@ const FilterModule = (function() {
                 if (!active) {
                     header.classList.toggle('filter-categoryfilter-accordion-header--disabled', catDisabled);
                 }
-                var toggleArea = header.querySelector('.filter-categoryfilter-accordion-header-togglearea');
-                if (toggleArea) toggleArea.style.pointerEvents = (active && !catInSolo) ? 'none' : '';
             }
 
             accordion.querySelectorAll('.filter-categoryfilter-accordion-option').forEach(function(opt) {
